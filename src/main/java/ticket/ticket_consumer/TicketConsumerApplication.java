a1869c2bad2e7e09cd45bbab8b0680fa090ed90a
28176 <jonathan.wk.lu@foxconn.com>
2024-12-21 16:31:27 +0800

    add spring init

---
+++ src/main/java/ticket/ticket_consumer/TicketConsumerApplication.java
@@ -0,0 +1,13 @@
+package ticket.ticket_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ticket/ticket_consumer/TicketConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package ticket.ticket_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
